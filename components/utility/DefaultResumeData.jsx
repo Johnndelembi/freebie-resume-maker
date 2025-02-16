e105930e6f885b0ba87993d1c5da06832ea181f4
John <williamjohnie61@gmail.com>
2025-02-16 16:46:25 +0300

    first commit

---
+++ components/utility/DefaultResumeData.jsx
@@ -0,0 +1,88 @@
+const DefaultResumeData = {
+  name: "JOHN NDELEMBI",
+  position: "Full Stack Developer",
+  contactInformation: "+255 625232734",
+  email: "williamjohnie61@gmail.com",
+  address: "Dar-es-Salaam -Tanzania",
+  profilePicture: "",
+  socialMedia: [
+    {
+      socialMedia: "Github",
+      link: "github.com//Johnndelembi",
+    },
+    {
+      socialMedia: "LinkedIn",
+      link: "linkedin.com/in/Johnndelembi",
+    },
+    {
+      socialMedia: "Website",
+      link: "johnny-dev.onrender.com",
+    },
+  ],
+  summary: "A Full Stack Developer with 2+ years of experience in web development, specializing in building dynamic and responsive web applications. Passionate about Backend dev, DevOps and Quantum Computing. with a keen interest in decentralized technologies and automation in the development process. Skilled in both front-end and back-end development, with a focus on delivering efficient and scalable solutions.",
+  education: [
+    {
+      "school": "University Of Dar-es-Salaam",
+      "degree": "Bachelors in Statistics",
+      "startYear": "2023-11-01",
+      "endYear": "2026-07-01"
+    },
+  ],
+  workExperience: [
+    {
+      "company": "Company Name",
+      "position": "Front End Developer",
+      "description": "company name is a Kariakoo based software company that offers services like Web Developement , App Development ,Digital Marketing , Business card.",
+      "keyAchievements": "Created and maintained 4 web applications for numerous clients.\nEnsured that the user interfaces and user experience of the software applications developed by the team met at least 80% of users expectations.",
+      "startYear": "2024-06-01",
+      "endYear": "2024-09-01"
+    },
+    {
+      "company": "MERN Auth App",
+      "position": "Full Stack Developer",
+      "description": "A full-featured authentication app using OAuth, Firebase for Gmail login, MySQL for the database, and Redux for global state management.The frontend is built with Bootstrap, providing a responsive and user-friendly interface.",
+      "keyAchievements": "Implemented OAuth and Firebase login features for Gmail authentication.\nIntegrated MySQL as a database solution for storing user information.",
+      "startYear": "2024-08-22",
+      "endYear": "2023-09-09"
+    },
+    {
+      "company": "MTABEAPP/MTABEWEB",
+      "position": "Simple Selcom Payment Funds Disbursment",
+      "description": "A simple selcom application built with Bootstrap for the frontend, PostgreSQL for the database, and REST framework for API calls",
+      "keyAchievements": "Developed secure selcom payment disbursment system.\nIntegrated Mobile Networks validation to ensure reliable data handling and funds transfer.\nImplemented a clean and responsive dashboard UI using Bootstrap",
+      "startYear": "2025-02-01",
+      "endYear": "2025-2-14"
+    }
+  ],
+  projects: [],
+  skills: [
+    {
+      title: "Technical Skills",
+      skills: [
+        "HTML", "CSS","JavaScript", "React", "Bootstrap", "Tailwind", "Nodejs","Express", "SQL", "TypeScript", "Nextjs","Python", "Docker","Git version control"
+      ]
+    },
+    {
+      title: "Soft Skills",
+      skills: [
+        "Collaboration", "Problem-solving", "Communication", "Time management", "Result-oriented"
+      ]
+    },
+    {
+      title: "Additional Skills",
+      skills: [
+        "Teaching tech-related stuffs", "Data Analysis and Interpretation", "WABots"
+      ] 
+    }
+  ],
+  languages: [
+    "English",
+    "Kiswahili"
+  ],
+  certifications: [
+    "Youtube, W3s Schools full stack web development",
+    "Many cerfications idk",
+  ],
+};
+
+export default DefaultResumeData;
